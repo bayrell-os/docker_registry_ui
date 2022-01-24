6de067bb5a1befe6ef10b5180b61ea209a579598
Ildar Bikmamatov <ildar@bayrell.org>
2022-01-24 10:44:12 +0000

    add arch

--- build.sh
+++ build.sh
@@ -17,17 +17,12 @@ case "$1" in
 	
 	amd64)
 		docker build ./ -t bayrell/docker_registry_ui:$VERSION-$SUBVERSION-amd64 \
-			--file stages/Dockerfile --build-arg ARCH=-amd64
+			--file stages/Dockerfile --build-arg ARCH=amd64/
 	;;
 	
 	arm64v8)
 		docker build ./ -t bayrell/docker_registry_ui:$VERSION-$SUBVERSION-arm64v8 \
-			--file stages/Dockerfile --build-arg ARCH=-arm64v8
-	;;
-	
-	arm32v7)
-		docker build ./ -t bayrell/docker_registry_ui:$VERSION-$SUBVERSION-arm32v7 \
-			--file stages/Dockerfile --build-arg ARCH=-arm32v7
+			--file stages/Dockerfile --build-arg ARCH=arm64v8/
 	;;
 	
 	manifest)
@@ -35,25 +30,21 @@ case "$1" in
 		
 		docker push bayrell/docker_registry_ui:$VERSION-$SUBVERSION-amd64
 		docker push bayrell/docker_registry_ui:$VERSION-$SUBVERSION-arm64v8
-		docker push bayrell/docker_registry_ui:$VERSION-$SUBVERSION-arm32v7
 		
 		docker manifest create bayrell/docker_registry_ui:$VERSION-$SUBVERSION \
 			--amend bayrell/docker_registry_ui:$VERSION-$SUBVERSION-amd64 \
-			--amend bayrell/docker_registry_ui:$VERSION-$SUBVERSION-arm64v8 \
-			--amend bayrell/docker_registry_ui:$VERSION-$SUBVERSION-arm32v7
+			--amend bayrell/docker_registry_ui:$VERSION-$SUBVERSION-arm64v8
 		docker manifest push bayrell/docker_registry_ui:$VERSION-$SUBVERSION
 		
 		docker manifest create bayrell/docker_registry_ui:$VERSION \
 			--amend bayrell/docker_registry_ui:$VERSION-$SUBVERSION-amd64 \
-			--amend bayrell/docker_registry_ui:$VERSION-$SUBVERSION-arm64v8 \
-			--amend bayrell/docker_registry_ui:$VERSION-$SUBVERSION-arm32v7
+			--amend bayrell/docker_registry_ui:$VERSION-$SUBVERSION-arm64v8
 		docker manifest push bayrell/docker_registry_ui:$VERSION
 	;;
 	
 	all)
 		$0 amd64
 		$0 arm64v8
-		$0 arm32v7
 		$0 manifest
 	;;
 	
